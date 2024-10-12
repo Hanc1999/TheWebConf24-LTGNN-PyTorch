52fff8ae2bde491d529f827ccd691eecb173a8dd
2023dfff <1374490218@qq.com>
2024-10-12 22:26:18 +0800

    add dataset

---
+++ data/instacart_full/sort.py
@@ -0,0 +1,21 @@
+# read tri_graph_uidx2pidx_valid.json and sort them according to the key
+
+import json
+import os
+
+def json_sort(json_file_path, txt_file_path):
+    with open(json_file_path, 'r') as json_file:
+        data = json.load(json_file)
+
+        # sort the data by converting keys to integers
+        data = dict(sorted(data.items(), key=lambda item: int(item[0])))
+
+    with open(txt_file_path, 'w') as txt_file:
+        for key, values in data.items():
+            line = f"{key} " + " ".join(map(str, values)) + "\n"
+            txt_file.write(line)
+
+json_file_path = '../instacart_full/tri_graph_uidx2tidx_train.json'
+txt_file_path = 'train.txt'
+
+json_sort(json_file_path, txt_file_path)
